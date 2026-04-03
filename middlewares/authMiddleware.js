762ec7a36fbbcdeefb7c6080d013166214e36c3d
Garv1234976 <garvgarg12@gmail.com>
2026-04-03 17:35:27 +0530

    change

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -19,7 +19,7 @@ async function authMiddleware(req, res, next) {
     const token = req.cookies?.token || req.headers.authorization?.split(' ')[1];
 
     if (!token) {
-        return res.status(401).json({ error: "Unauthorized" });
+        return res.status(401).json({ error: "Unauthorized hai ye " });
     }
 
     try {
